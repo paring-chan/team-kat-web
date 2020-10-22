0579811be81fc26d7dcc60a987a23eaae62afaf8
kiki7000 <devkiki7000@gmail.com>
2020-10-22 11:43:59 +0900

    대문자 아니라고...

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const styledimage = styled.img`
+const StyledImage = styled.img`
     width: 80vw;
     margin-left: 10vw;
     margin-right: 10vw;
@@ -9,7 +9,7 @@ const styledimage = styled.img`
 `
 
 const Main = () => (
-    <styledimage src = {`${process.env.PUBLIC_URL}/unknown.png`} alt = 'test'/>
+    <StyledImage src = {`${process.env.PUBLIC_URL}/unknown.png`} alt = 'test'/>
 )
 
 export default Main
