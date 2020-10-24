179142ff2ce04e11e2a9fa8822fe617e03b9939c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-24 13:50:48 +0000

    Deploy Team-Kat/Team-Kat.github.io to github.com/Team-Kat/Team-Kat.github.io.git:gh-pages

---
+++ precache-manifest.d0e91b67079ea93cf38960f885dc3806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8309a66f15b256d2164500766dc8e80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44962f357359eb64c393",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "9edaf82e90478575d747",
+    "url": "/static/css/main.078a1f8f.chunk.css"
+  },
+  {
+    "revision": "44962f357359eb64c393",
+    "url": "/static/js/2.11c7a867.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.11c7a867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edaf82e90478575d747",
+    "url": "/static/js/main.5310ad2d.chunk.js"
+  },
+  {
+    "revision": "93acbfa5ff37092dfcc3",
+    "url": "/static/js/runtime-main.ac6acc1e.js"
+  }
+]);
