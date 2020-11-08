4d3ca656649dd3891687bfbfdf482fd820b73045
kiki7000 <devkiki7000@gmail.com>
2020-11-08 19:37:32 +0900

    Routes setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
-const App = () => (
-    <h1>Hello World</h1>
-)
+import { init, refresh } from 'aos'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Main from './routes/Main'
+import Tos from './routes/Tos'
+import Privacy from './routes/Privacy'
+import NotFound from './routes/NotFound'
+
+const App = () => {
+    useEffect(() => {
+        init()
+        refresh()
+    }, [])
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path = '/' component = {Main} />
+                <Route exact path = '/tos' component = {Tos} />
+                <Route exact path = '/privacy' component = {Privacy} />
+                <Route component = {NotFound} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
 
 export default App
