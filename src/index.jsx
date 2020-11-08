331e1da2ff9e51291b6a4c4007e7a55be532b7c3
kiki7000 <devkiki7000@gmail.com>
2020-11-08 19:27:38 +0900

    Initial Commit Again

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<div>Hello, world!</div>, document.querySelector('#root'))
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.querySelector('#root')
+)
